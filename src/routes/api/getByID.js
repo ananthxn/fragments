87f872c195c7f1b7e0143625e69ea353bcdd5ce7
Anantha Krishnan <ananthan.k.arun@gmail.com>
2024-02-12 21:37:24 -0500

    added loggings

--- src/routes/api/getByID.js
+++ src/routes/api/getByID.js
@@ -14,14 +14,15 @@ router.get('/fragments/:id', async (req, res) => {
     const fragment = await Fragment.byId(ownerId, id);
 
     if (!fragment) {
+      console.warn('Fragment not found with getById ', { ownerId, fragmentId: id });
       return res.status(404).json(createErrorResponse('Fragment not found', 404));
     }
     const data = await fragment.getData();
+    console.info('Fragment data retrieved using with getById ', { ownerId, fragmentId: id });
 
     res.set('Content-Type', fragment.type);
     res.status(200).send(data);
   } catch (error) {
-    console.error(error);
     res.status(404).json(createErrorResponse(404, error));
   }
 });
--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -4,9 +4,11 @@ const contentType = require('content-type');
 const { Fragment } = require('../../model/fragment');
 const { createSuccessResponse, createErrorResponse } = require('../../response');
 const router = express.Router();
+const logger = require('../../logger');
 
-router.post('/fragments',  async (req, res, next) => {
+router.post('/fragments', async (req, res, next) => {
   try {
+    logger.debug('POST request', { body: req.body });
     if (!Buffer.isBuffer(req.body)) {
       return res.status(415).json(createErrorResponse(new Error('Unsupported content type'), 415));
     }
@@ -17,8 +19,11 @@ router.post('/fragments',  async (req, res, next) => {
     });
 
     await fragment.save();
+    logger.info(`POST fragment saved with ID: ${fragment.id}`);
+
     await fragment.setData(req.body);
-  
+    logger.debug('POST fragment data is set');
+
     res.set('Content-Type', fragment.type);
     res.setHeader('Location', `http://${req.headers.host}/v1/fragments/${fragment.id}`);
     res.status(201).json(createSuccessResponse({
@@ -26,6 +31,7 @@ router.post('/fragments',  async (req, res, next) => {
       fragment: fragment
     }));
   } catch (error) {
+    logger.error('error during POST fragment');
     next(createErrorResponse(error, 500));
   }
 });
